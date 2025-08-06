a21a2f532fefb0644f9a32f08ea464209882e397
hyfff372 <1163206775@qq.com>
2025-08-06 12:13:39 +0000

    complete lab01 & lab02

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    struct node *tortoise, *hare;
+    if (!head){
+        return 0;
+    }
+    tortoise = hare = head;
+    while(hare != NULL && hare->next != NULL){
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (tortoise == hare){
+            return 1;
+        }
+    }
     return 0;
+    
 }
--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -6,5 +6,11 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t x0 = (*reg)&1;
+    uint16_t x2 = (*reg >> 2)&1;
+    uint16_t x3 = (*reg >> 3)&1;
+    uint16_t x5 = (*reg >> 5)&1;
+    uint16_t x = (x0^x2^x3^x5);
+    (*reg) = ((*reg) >> 1) ^ (x << 15);
 }
 
